6bd701cbfe5ae07499c219b10380094e8bd0db2b
Jakub Krekora <jakub_krekora@wp.pl>
2017-03-27 22:06:52 +0200

    personal data button added

---
+++ www/js/pgb.js
@@ -0,0 +1,34 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function personalData() {
+
+	info =  'This is my personal data' + '\n' +
+			'=====' + '\n' +
+			'Name    : Jakub'     + '\n' + 
+			'Surname : Krekora'   + '\n' + 
+			'E-mail: jakub_krekora@wp.pl ' + '\n';
+
+	navigator.notification.alert(info);
+	
+}
